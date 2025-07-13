a35ff48d17de553f3d739fd56d65d71ef4b11a09
luca toscana <lucatscn@gmail.com>
2025-07-13 03:45:13 +0200

    fix reszeict

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-
+// Se siamo stati reindirizzati da 404.html, ripristina il path originale
+const redirect = new URLSearchParams(window.location.search).get('redirect')
+if (redirect) {
+  window.history.replaceState(null, '', redirect)
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
